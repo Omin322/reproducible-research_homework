0b1bdbf8faf9274fe33c7b8e367cc8f65af4709f
Omin322 <michael.pilarinos@pmb.ox.ac.uk>
2024-12-12 18:24:01 +0000

    commit

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -6,6 +6,8 @@ library(gridExtra)
 
 random_walk  <- function (n_steps) {
   
+  set.seed(1)
+  
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
   df[1,] <- c(0,0,1)
